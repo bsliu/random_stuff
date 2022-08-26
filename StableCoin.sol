4f0a19b2923b1ec412e4d20bbca3aac168cb0568
bsliu <liubiah.uiuc@gmail.com>
2022-08-26 14:50:15 -0400

    Add files via upload

---
+++ StableCoin.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StableCoin is ERC20 {
+    uint constant _initial_supply = 100 * (10**18);
+    constructor() ERC20("StableCoin", "StableCoin") {
+        _mint(msg.sender, _initial_supply);
+    }
+
+    function mint(uint256 amount) public returns (bool) {
+        _mint(msg.sender, amount);
+        return true;
+    }
+}
